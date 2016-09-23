8fe9c75f000157fb5a08bac1905675306401ecb4
Jason Ly <ja_ly@apple.com>
2016-09-22 17:18:40 -0700

    [TASK] Removing alerts to replace it with a modal

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-  var timer = new Timer();
+var timer = new Timer();
 var game = new Hangman();
 var answer = game.answer();
+var overlay = new Winlose();
 
 game.init();
 
+overlay.init();
+
 // timer.setTime(5);
 // timer.init(answer);
+
+
---
+++ scripts/winlose.js
@@ -0,0 +1,31 @@
+var Winlose = function () {
+  var createOverlay = function() {
+      var _overlay = document.createElement('div');
+      var _overlayContent = document.createElement('div');
+      var body = document.querySelector('body');
+
+      _overlay.classList.add('overlay');
+      _overlayContent.classList.add('overlay-content');
+
+      _overlay.appendChild(_overlayContent);
+
+      body.appendChild(_overlay);
+
+      whatHappened();
+  };
+
+  var whatHappened = function() {
+    var _title = document.createElement('h1');
+    var _overlayContent = document.querySelector('.overlay-content');
+
+    _title.innerHTML = 'OH?';
+
+    _overlayContent.appendChild(_title);
+
+    return _overlayContent;
+  };
+
+  return {
+    init: createOverlay
+  };
+};
